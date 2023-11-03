46ac746213bf756a51e420ecae7cada7879c769f
licorice <1347068572@qq.com>
2023-11-03 14:08:12 +0800

    first Commint

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @Author: LuoYuanLin
+ * @Description: TODO
+ * @DateTime: 2023/11/3 14:07
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
